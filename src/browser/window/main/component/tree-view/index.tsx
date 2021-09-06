f08a0bc0c64d3c11f75d80664ad5aa690a69ea30
septlog <septlog@qq.com>
2021-09-06 23:54:34 +0800

    collapse

--- src/browser/window/main/component/tree-view/index.tsx
+++ src/browser/window/main/component/tree-view/index.tsx
@@ -1,5 +1,11 @@
 import { observer } from 'mobx-react-lite';
-import { ReactElement, ReactNode, useState } from 'react';
+import {
+  ReactElement,
+  ReactNode,
+  useEffect,
+  useLayoutEffect,
+  useState,
+} from 'react';
 import { treeStore } from '../../store';
 interface Props {
   children?: any;
@@ -9,13 +15,18 @@ const TreeView = () => {
     <TreeItem
       data={{
         value: 'a',
+        collapse: false,
         children: [
           { value: 'b' },
           {
             value: 'c',
             children: [
               { value: 'd' },
-              { value: 'e', children: [{ value: 'f' }, { value: 'g' }] },
+              {
+                value: 'e',
+                collapse: true,
+                children: [{ value: 'f' }, { value: 'g' }],
+              },
             ],
           },
         ],
@@ -25,16 +36,25 @@ const TreeView = () => {
 };
 
 const TreeItem = ({ data }) => {
-  const [collapse, setCollapse] = useState(true);
+  const [collapse, setCollapse] = useState(false);
+  useLayoutEffect(() => {
+    if (data.children && data.collapse) {
+      console.log(data.collapse);
+      setCollapse(data.collapse);
+    }
+    console.log('exe');
+  }, []);
   if (data.children) {
     return (
       <ul style={{ listStyleType: 'none' }}>
         <p
           onClick={() => {
-            setCollapse(!collapse);
+            // setCollapse(!collapse);
+            data.collapse = !data.collapse;
+            setCollapse(data.collapse);
           }}
         >
-          {data.value}
+          {collapse ? <em>👉</em> : <em>👇</em>} {data.value}
         </p>
         {!collapse &&
           data.children.map((item, index) => {
